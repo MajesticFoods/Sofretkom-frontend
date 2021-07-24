3cfa1b9ebb555311da95c591963c12ddcbe35778
OdehYazan <eng.yazanodeh@gmail.com>
2021-07-24 15:36:57 +0300

    form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Recipe from "./components/Recipe";
 
+
+
 class App extends Component {
   render() {
     return (
       <>
+    
         <Recipe />
       </>
     );
